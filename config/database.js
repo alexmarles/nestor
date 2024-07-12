65e12417df3c9575bb37853329b3e8e2395783b9
Alex Marles Alcaraz <alex.marles@king.com>
2024-07-12 15:05:25 +0200

    Up to speed

--- config/database.js
+++ config/database.js
@@ -1,21 +1,11 @@
-const firebase = require('firebase/app');
-require('firebase/database');
+const admin = require('firebase-admin');
+const serviceAccount = require('../FirebaseService.json');
 
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+});
 
-const config = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: `${process.env.FIREBASE_PROJECT_ID}.firebaseapp.com`,
-    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
-    messagingSenderId: process.env.FIREBASE_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-};
-
-firebase.initializeApp(config);
-
-// Get a reference to the database service
 module.exports = {
-    database: firebase.database(),
+    database: admin.database(),
 };
