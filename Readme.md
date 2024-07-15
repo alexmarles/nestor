4519b5ec4c6cc154db2cf7a2e2014bbf1010435c
Alex Marles Alcaraz <alex.marles@king.com>
2024-07-15 15:41:41 +0200

    Moved bot to Cloud Functions

--- Readme.md
+++ Readme.md
@@ -1,1 +1,15 @@
 # Nestor Nichols Bot
+
+Tutorial from Roman Akhromieiev [here](https://medium.com/firebase-developers/building-a-telegram-bot-with-firebase-cloud-functions-and-telegraf-js-5e5323068894)
+
+## Deploy
+
+```sh
+firebase deploy --only functions
+```
+
+## Set webhook
+
+```sh
+curl --location --request POST 'https://api.telegram.org/bot<BOT-TOKEN>/setWebhook?url=<GCLOUD-FUNCTION-URL>' --header 'Accept: application/json'
+```
--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,12 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-const { onRequest } = require('firebase-functions/v2/https');
-const logger = require('firebase-functions/logger');
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 const functions = require('firebase-functions');
 const { Telegraf } = require('telegraf');
-const { initNestor } = require('./nestor');
+const { initNestor } = require('./initNestor');
+require('dotenv').config();
 
-const bot = new Telegraf(functions.config().telegram.token, {
+// functions.config().telegram.token
+// or
+// process.env.BOT_TOKEN
+const bot = new Telegraf(process.env.BOT_TOKEN, {
     telegram: { webhookReply: true },
 });
 
@@ -32,13 +16,20 @@ bot.catch((err, ctx) => {
     return ctx.reply(`Ooops, encountered an error for ${ctx.updateType}`, err);
 });
 
+// initialize the commands
 initNestor(bot);
 
+// copy every message and send to the user
+bot.on('message', ctx => ctx.telegram.sendCopy(ctx.chat.id, ctx.message));
+
 // handle all telegram updates with HTTPs trigger
-exports.nestorBot = functions.https.onRequest(async (request, response) => {
-    functions.logger.log('Incoming message', request.body);
-    return await bot.handleUpdate(request.body, response).then(rv => {
-        // if it's not a request from the telegram, rv will be undefined, but we should respond with 200
-        return !rv && response.sendStatus(200);
+exports.nestorBot = functions
+    .region(process.env.REGION)
+    .runWith({ minInstances: Number.parseInt(process.env.MIN_INSTANCES) })
+    .https.onRequest(async (request, response) => {
+        functions.logger.log('Incoming message', request.body);
+        return await bot.handleUpdate(request.body, response).then(rv => {
+            // if it's not a request from the telegram, rv will be undefined, but we should respond with 200
+            return !rv && response.sendStatus(200);
+        });
     });
-});
